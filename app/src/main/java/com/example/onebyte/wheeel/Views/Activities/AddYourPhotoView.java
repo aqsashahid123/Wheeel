4317f9ce9f86900967450076e7a4c4b6aa1eab9a
aqsashahid123 <aqsa65020@gmail.com>
2018-08-25 18:29:06 +0500

    commit by aqsa

--- app/src/main/java/com/example/onebyte/wheeel/Views/Activities/AddYourPhotoView.java
+++ app/src/main/java/com/example/onebyte/wheeel/Views/Activities/AddYourPhotoView.java
@@ -3,11 +3,22 @@ package com.example.onebyte.wheeel.Views.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AddYourPhotoView extends AppCompatActivity {
+import com.anton46.stepsview.StepsView;
+import com.example.onebyte.wheeel.R;
 
+public class AddYourPhotoView extends AppCompatActivity {
+    StepsView stepsView;
+    String[] arrSteps = {"step1","step2","step3"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_your_photo_view);
+        stepsView = (StepsView) findViewById(R.id.stepsView);
+        stepsView.setLabels(arrSteps)
+                .setBarColorIndicator(getResources().getColor(R.color.colorDarkPurple))
+                .setProgressColorIndicator(getResources().getColor(R.color.colorWhite))
+                .setLabelColorIndicator(getResources().getColor(R.color.colorWhite))
+                .setCompletedPosition(1)
+                .drawView();
     }
 }
